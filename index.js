1475bfec74a9c76219f407e9d6c017c5feb0bcc3
Herodotus97 <herodotus9719@gmail.com>
2025-01-29 15:37:09 +0000

    regular commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 
 // Our own middleware
@@ -94,7 +95,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
-console.log("HERE_________", process.env.PORT);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
